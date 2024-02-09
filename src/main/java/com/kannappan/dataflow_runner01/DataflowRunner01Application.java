a354b43c00855d3b26991840ad3a734aa9117688
KannappanRM <kannappanmkce@gmail.com>
2024-02-09 12:43:54 +0530

    Commit01_Making the code as executable
    Commit01_Making the code as executable

---
+++ src/main/java/com/kannappan/dataflow_runner01/DataflowRunner01Application.java
@@ -0,0 +1,13 @@
+package com.kannappan.dataflow_runner01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataflowRunner01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataflowRunner01Application.class, args);
+	}
+
+}
